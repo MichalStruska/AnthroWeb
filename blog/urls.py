e996cd54c7c3e08599c1e8b225bf020e3116c27f
MichalStruska <struska1408@gmail.com>
2022-06-15 01:39:16 +0200

    added primitive version of separated post pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='blog-home'),
+    path('article/<int:pk>', views.PostDetail.as_view(), name='article-detail'),
     path('about/', views.about, name='blog-about'),
     path('news/', views.news, name='blog-news'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,9 +6,9 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/home.html'
 
-# class PostDetail(generic.DetailView):
-#     model = Post
-#     template_name = 'post_detail.html'
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/article_detail.html'
 
 def home(request):
     # context = {
